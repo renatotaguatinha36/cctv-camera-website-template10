e436a1b4c11d90ac0308298a8c7217b02ccbf737
Renato Alves Soares <renatoguara2020@gmail.com>
2023-01-18 09:08:39 -0300

    ok

--- mail/contact.php
+++ mail/contact.php
@@ -33,12 +33,12 @@ $subject = filter_input(INPUT_POST, 'subject', FILTER_SANITIZE_SPECIAL_CHARS);
 ########################################## PHPMailer#############################################
 $mail = new PHPMailer();
 
-$mail->SMTPDebug = 2; //Enable verbose debug output
+//$mail->SMTPDebug = 2; //Enable verbose debug output
 $mail->isSMTP(); //Send using SMTP
 $mail->Host = 'smtp.gmail.com'; //Set the SMTP server to send through
 $mail->SMTPAuth = true; //Enable SMTP authentication
 $mail->Username = 'renatoguara2020@gmail.com'; //SMTP username
-$mail->Password = 'agciqpqrqferdazp'; // password is optional //SMTP password 
+$mail->Password = 'agc'; // password is optional //SMTP password 
 // $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
 $mail->Port = 587;
 $mail->CharSet = 'UTF-8';
