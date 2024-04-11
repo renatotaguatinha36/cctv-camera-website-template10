6ffc6c739222c28f1099cdc520d4fdd98ed9ceeb
renatoguara2020 <renatoguara2020@gmail.com>
2024-04-11 14:46:42 -0300

    ok

--- mail/connection.php
+++ mail/connection.php
@@ -4,6 +4,8 @@ $username = "username";
 $password = "password";
 $dbname = "myDBPDO";
 
+$id = $_GET['id'];
+
 if (empty($_POST["firstname"]) || empty($_POST["lastname"]) || empty($_POST["email"]) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
 
     echo "Please enter your first name";
