6faced97495a3767530a1ac4d550cf905717557d
renatoguara2020 <renatoguara2020@gmail.com>
2024-04-11 17:44:56 -0300

    OK

--- mail/connection.php
+++ mail/connection.php
@@ -18,7 +18,7 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-  $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES (:id, :firstname, :lastname, :email)");
+  $stmt = $conn->prepare("INSERT INTO MyGuests (id, firstname, lastname, email) VALUES (:id, :firstname, :lastname, :email)");
   $stmt->bindValue(':firstname', $firstname, PDO::PARAM_STR);
   $stmt->bindValue(':lastname', $lastname, PDO::PARAM_STR);
   $stmt->bindValue(':email', $email, PDO::PARAM_STR);
